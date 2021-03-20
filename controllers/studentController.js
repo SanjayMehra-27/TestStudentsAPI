011519e44cbad8a9dc353341617c187de1c88b39
Sanjay Mehra <mehra271198@gmail.com>
2021-03-21 01:07:08 +0530

    add error message feature

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -12,7 +12,7 @@ router.use(express.urlencoded({ extended: true }));
 router.get('/students', async (req, res) => {
 
 
-    let result = await fetch(
+    await fetch(
         "http://localhost:3000/students", {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
@@ -25,8 +25,15 @@ router.get('/students', async (req, res) => {
             })
         })
         .catch((err) => {
-            console.log(err)
+            if (err.code === 'ECONNREFUSED') {
+                console.err(err)
+                return res.render('./layouts/students/students.hbs', {
+                    error: ' Some Error Occurred During The Fetching Students Records.',
+                    show: 'show'
+                })
+            }
         });
+
 });
 
 // GET : Single Student 
@@ -44,6 +51,9 @@ router.get('/students/edit/:id', async (req, res) => {
 
         })
         .catch((err) => console.log(err));
+    return res.render('./layouts/students/edit.hbs', {
+        student: data,
+    })
 })
 
 // POST : Create Student
@@ -78,16 +88,16 @@ router.post('/students/:id', async (req, res) => {
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
-
+            return res.redirect('http://localhost:8000/api/students');
         })
-        .catch((err) => console.log(err));
-    return res.redirect('http://localhost:8000/api/students');
+        .catch((err) => { console.log(err) });
+
 });
 
 // DELETE : Delete particular Student 
-router.get('/students/delete/:id', (req, res) => {
+router.get('/students/delete/:id', async (req, res) => {
 
-    fetch('http://localhost:3000/students/' + req.params.id, {
+    await fetch('http://localhost:3000/students/' + req.params.id, {
         method: 'DELETE',
         body: JSON.stringify(req.body),
         headers: { 'Content-Type': 'application/json' },
