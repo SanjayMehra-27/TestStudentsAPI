d3ab352a3d91a442feb1f2f9d664b3e3c7ad4a97
SanjayMehra-27 <mehra271198@gmail.com>
2021-03-21 11:14:25 +0530

    updated

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -67,7 +67,6 @@ router.post('/students', async (req, res) => {
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
-
             return res.redirect('students');
 
         })
