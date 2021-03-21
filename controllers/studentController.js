59bc074668e1dcb866b8ffa0035f0fed523fc440
SanjayMehra-27 <mehra271198@gmail.com>
2021-03-21 10:22:15 +0530

    update

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,28 +1,18 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require('path');
-
 const fetch = require('node-fetch');
-var url = require('url');
 const router = express.Router();
-
-const testAPIurl = 'http://localhost:3000/students'
-
-
 
 // body-parser
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-// router.get('/students/update/:id',(req,res)=>{
-//     return router.put(req,res);
-// })
 
 // GET STUDENTS LIST
 router.get('/students', async (req, res) => {
 
 
-    let result = await fetch(
+    await fetch(
         "http://localhost:3000/students", {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
@@ -35,8 +25,15 @@ router.get('/students', async (req, res) => {
             })
         })
         .catch((err) => {
-            console.log(err)
+            if (err.code === 'ECONNREFUSED') {
+                console.err(err)
+                return res.render('./layouts/students/students.hbs', {
+                    error: ' Some Error Occurred During The Fetching Students Records.',
+                    show: 'show'
+                })
+            }
         });
+
 });
 
 // GET : Single Student 
@@ -54,6 +51,9 @@ router.get('/students/edit/:id', async (req, res) => {
 
         })
         .catch((err) => console.log(err));
+    return res.render('./layouts/students/edit.hbs', {
+        student: data,
+    })
 })
 
 // POST : Create Student
@@ -88,16 +88,16 @@ router.post('/students/:id', async (req, res) => {
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
-
+            return res.redirect('http://localhost:8000/api/students');
         })
-        .catch((err) => console.log(err));
-    return res.redirect('http://localhost:8000/api/students');
+        .catch((err) => { console.log(err) });
+
 });
 
 // DELETE : Delete particular Student 
-router.get('/students/delete/:id', (req, res) => {
+router.get('/students/delete/:id', async (req, res) => {
 
-    fetch('http://localhost:3000/students/' + req.params.id, {
+    await fetch('http://localhost:3000/students/' + req.params.id, {
         method: 'DELETE',
         body: JSON.stringify(req.body),
         headers: { 'Content-Type': 'application/json' },
